e0984b5add97fdaf3855ebecc858422cc9a3492c
Asmoday4949 <malik.fleury@bluewin.ch>
2019-04-17 14:01:27 +0200

    Add scripts for rigle

---
+++ Scripts/Ammo/RifleAmmo.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public class RifleAmmo : Ammo
+{
+    private static readonly int RADIUS = 5;
+    private static readonly int DAMAGE = 35;
+
+    private bool launched = false;
+
+    public RifleAmmo(): this(1, 1)
+    {
+        // Nothing
+    }
+
+    public RifleAmmo(int radius, int damage): base(radius, damage)
+    {
+    }
+
+    public override void _Ready()
+    {
+    }
+
+    public override void _Input(InputEvent inputEvent)
+    {
+        // Nothing
+    }
+
+    public override void Launch(Vector2 direction, int strength)
+    {
+        direction = direction.Normalized();
+        this.Mode = ModeEnum.Rigid;
+
+        this.ApplyImpulse(Vector2.Zero, direction * strength);
+
+        launched = true;
+    }
+}
---
+++ Scripts/Weapons/Rifle.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public class Rifle : Weapon
+{
+    private static readonly String WEAPON_NAME = "Rifle";
+    private static readonly String AMMO_SCENE_PATH = "res://Scenes/RifleAmmo.tscn";
+
+    private static readonly int STRENGTH_FACTOR = 5000;
+    private Position2D rifleHolder = null;
+
+    public Rifle(): base(WEAPON_NAME, null)
+    {
+    }
+
+    public override void _Ready()
+    {
+        rifleHolder = this.GetChild<Position2D>(0);
+    }
+
+    public override void Load()
+    {
+        PackedScene rifleAmmoScene = (PackedScene)ResourceLoader.Load(AMMO_SCENE_PATH);
+
+        RifleAmmo rifleAmmo = rifleAmmoScene.Instance() as RifleAmmo;
+        rifleHolder.AddChild(rifleAmmo);
+        this.WeaponAmmo = rifleAmmo;
+    }
+
+    public override void Shoot(int elapsedTime)
+    {
+        Vector2 direction = GetMouseDirection();
+
+        this.Load();
+
+        rifleHolder.RemoveChild(WeaponAmmo);
+        GetTree().GetRoot().AddChild(WeaponAmmo);
+        WeaponAmmo.SetGlobalPosition(rifleHolder.GlobalPosition);
+
+        RifleAmmo rifleAmmo = WeaponAmmo as RifleAmmo;
+        rifleAmmo.Launch(direction, STRENGTH_FACTOR);
+    }
+}
