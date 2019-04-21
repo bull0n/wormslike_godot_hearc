3504c9920bf01c825d675fd57557aead78a5c584
Asmoday4949 <malik.fleury@bluewin.ch>
2019-04-21 21:43:37 +0200

    Add simple collision exemple with grenade

--- Scripts/Ammo/Grenade.cs
+++ Scripts/Ammo/Grenade.cs
@@ -6,11 +6,13 @@ public class Grenade : Ammo
     private static readonly int RADIUS = 5;
     private static readonly int DAMAGE = 35;
 
+    private CollisionShape2D collisionObject;
+    private Area2D areaExplosion;
+
     private bool launched = false;
 
     public Grenade(): this(1, 1)
     {
-        // Nothing
     }
 
     public Grenade(int radius, int damage): base(radius, damage)
@@ -19,6 +21,10 @@ public class Grenade : Ammo
 
     public override void _Ready()
     {
+        collisionObject = (CollisionShape2D)this.GetNode("CollisionObject");
+        areaExplosion = (Area2D)this.GetNode("AreaExplosion");
+
+        Connect("body_entered", this, "OnBodyEnter");
     }
 
     public override void Launch(Vector2 direction, int strength)
@@ -30,5 +36,24 @@ public class Grenade : Ammo
         this.ApplyTorqueImpulse((direction * strength + Vector2.Down * GravityScale * Mass).Angle());
 
         launched = true;
+    }
+	
+	private void OnBodyEnter(object body)
+    {
+        if(!collisionObject.Disabled)
+        {
+            GD.Print("Touched");
+
+            foreach(Node node in areaExplosion.GetOverlappingBodies())
+            {
+                if(node is Character)
+                {
+                    Character character = node as Character;
+                    character.Health -= DAMAGE;
+                }
+            }
+
+            this.QueueFree();
+        }
     }
 }
